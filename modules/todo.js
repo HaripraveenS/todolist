667337c5294cbf9795d2f5849cd576902380ea13
HaripraveenS <haripraveen.s@students.iiit.ac.in>
2020-05-31 13:12:49 +0530

    added update function for todo

--- modules/todo.js
+++ modules/todo.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const con = require('./database');
 const mysql = require('mysql');
 const jwt = require('jsonwebtoken');
-
+const assert = require('assert');
 const {checkToken } = require('../auth/token');
-
-
+const EventEmitter = require('events');
+class dbquery extends EventEmitter { }
+const dbemit = new dbquery();
 router.get('/' ,checkToken, function (req , res){
     var token = req.get("authorization");
     token = token.slice(7);
@@ -36,7 +37,7 @@ router.post('/' ,checkToken, function (req , res){
     const payload = jwt.verify(token, "shhhh");
     console.log(req.body.title);
     var todet = req.body.title;
-    var todobody = req.body.bod;
+    var todobody = req.body.body;
     var uid1 = payload.details.id;
     con.getConnection( (err , connection) => {
     if(err) console.log(err);
@@ -60,6 +61,77 @@ router.post('/' ,checkToken, function (req , res){
 });
 });
 
+router.put('/' ,checkToken, function (req , res){
+    var token = req.get("authorization");
+    token = token.slice(7);
+    const payload = jwt.verify(token, "shhhh");
+    var oldtitle = req.body.oldtitle;
+    var newtitle = req.body.newtitle;
+    var newtodobody = req.body.newbody;
+    var uid1 = payload.details.id;
+    var actual_uid;
+    con.getConnection( (err , connection) => {
+    if(err) console.log(err);
+    console.log("connection established");
+    connection.query(`SELECT usid from todos WHERE title=?`,
+        [oldtitle],
+        function(err , results , feilds){
+            if(err){
+                console.log(err);
+                res.json({
+                    success:0,
+                    message:"error in connecting with database"
+                })
+            }
+            else {
+                if(results.length == 0)
+                {
+                    res.json({
+                        success:0,
+                        message:"no such todo exists"
+                    })
+                }
+                else{
+                    actual_uid = results[0].usid;
+                // console.log(actual_uid + "slafh");
+                // res.send("askldhf");
+                dbemit.emit('gotuid');
+                }
+            }
+        }
+    
+    )});
+    dbemit.on('gotuid', ()=> {
+        // console.log(actual_uid + "lslhdfslakdgh");
+    
+    // console.log(assert.equal(uid1,actual_uid));
+    if(uid1 != actual_uid){
+        res.json({
+            success:0,
+            message:"this is not your todo so you cannot modify it"
+        })
+    }
+    else {
+        con.getConnection( (err , connection) => {
+        connection.query(`UPDATE todos SET title=? , body=? , usid=?  WHERE title=?` ,
+        [
+            newtitle,
+            newtodobody,
+            uid1,
+            oldtitle
+        ],
+        function(err , results , feilds){
+        if(err) {console.log(err);}
+        // res.send(JSON.stringify(results))
+        // res.send(results[0].title + results[0].body);
+        else {
+            res.send("todo updated successfully");
+        }
+    });
+    });
 
+    }
+    });
+});
 
 module.exports = router;
